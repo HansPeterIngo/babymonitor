451c6700a772c3aa43b20fd60f68d5e652fe4ccf
HansPeterIngo <dark5@gmx.de>
2015-11-29 19:13:39 +0100

    Update keepalive.sh

--- keepalive.sh
+++ keepalive.sh
@@ -3,14 +3,14 @@
 while true
 do
   eval $1 &
-  echo $! > /home/pi/alive_pid
+  echo $! > ~/alive_pid
   i=0
-  while test $i -lt 3600 -a -e /proc/`cat /home/pi/alive_pid`
+  while test $i -lt 3600 -a -e /proc/`cat ~/alive_pid`
   do
     sleep 1
     i=$(($i+1))
   done
-  kill `cat /home/pi/alive_pid`
+  kill `cat ~/alive_pid`
   kill `pidof sox`
 done
 
